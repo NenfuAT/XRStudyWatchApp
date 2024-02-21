092e6279697d7db19a946b0a0e4b024e1e60c225
k21091 <k21091kk@aitech.ac.jp>
2024-02-21 12:08:39 +0900

    Initial commit

---
+++ app/src/main/java/com/k21091/xrstudywatchapp/MainActivity.kt
@@ -0,0 +1,185 @@
+package com.k21091.xrstudywatchapp
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.ar.core.Config
+import com.google.ar.core.Session
+import com.google.ar.core.examples.kotlin.helloar.HelloArView
+import com.google.ar.core.exceptions.CameraNotAvailableException
+import com.google.ar.core.exceptions.UnavailableApkTooOldException
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException
+import com.k21091.xrstudywatchapp.kotlin.common.helpers.ARCoreSessionLifecycleHelper
+import com.k21091.xrstudywatchapp.common.helpers.CameraPermissionHelper
+import com.k21091.xrstudywatchapp.common.helpers.DepthSettings
+import com.k21091.xrstudywatchapp.common.helpers.FullScreenHelper
+import com.k21091.xrstudywatchapp.common.helpers.InstantPlacementSettings
+import com.k21091.xrstudywatchapp.common.samplerender.SampleRender
+import com.k21091.xrstudywatchapp.kotlin.ar.HelloArRenderer
+import com.k21091.xrstudywatchapp.ui.theme.XRStudyWatchAppTheme
+
+class MainActivity : ComponentActivity() {
+    companion object {
+        private const val TAG = "HelloArActivity"
+    }
+
+    lateinit var arCoreSessionHelper: ARCoreSessionLifecycleHelper
+    lateinit var view: HelloArView
+    lateinit var renderer: HelloArRenderer
+
+    val instantPlacementSettings = InstantPlacementSettings()
+    val depthSettings = DepthSettings()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Setup ARCore session lifecycle helper and configuration.
+        arCoreSessionHelper = ARCoreSessionLifecycleHelper(this)
+        // If Session creation or Session.resume() fails, display a message and log detailed
+        // information.
+        arCoreSessionHelper.exceptionCallback =
+            { exception ->
+                val message =
+                    when (exception) {
+                        is UnavailableUserDeclinedInstallationException ->
+                            "Please install Google Play Services for AR"
+                        is UnavailableApkTooOldException -> "Please update ARCore"
+                        is UnavailableSdkTooOldException -> "Please update this app"
+                        is UnavailableDeviceNotCompatibleException -> "This device does not support AR"
+                        is CameraNotAvailableException -> "Camera not available. Try restarting the app."
+                        else -> "Failed to create AR session: $exception"
+                    }
+                Log.e(TAG, "ARCore threw an exception", exception)
+            }
+
+        // Configure session features, including: Lighting Estimation, Depth mode, Instant Placement.
+        arCoreSessionHelper.beforeSessionResume = ::configureSession
+        lifecycle.addObserver(arCoreSessionHelper)
+
+        // Set up the Hello AR renderer.
+        renderer = HelloArRenderer(this)
+        lifecycle.addObserver(renderer)
+
+        // Set up Hello AR UI.
+        view = HelloArView(this)
+        lifecycle.addObserver(view)
+        //setContentView(view.root)
+        setContent {
+            ARview()
+        }
+
+        // Sets up an example renderer using our HelloARRenderer.
+        SampleRender(view.surfaceView, renderer, assets)
+
+        depthSettings.onCreate(this)
+        instantPlacementSettings.onCreate(this)
+    }
+
+    // Configure the session, using Lighting Estimation, and Depth mode.
+    fun configureSession(session: Session) {
+        session.configure(
+            session.config.apply {
+                lightEstimationMode = Config.LightEstimationMode.ENVIRONMENTAL_HDR
+                cloudAnchorMode = Config.CloudAnchorMode.ENABLED
+
+                // Depth API is used if it is configured in Hello AR's settings.
+                depthMode =
+                    if (session.isDepthModeSupported(Config.DepthMode.AUTOMATIC)) {
+                        Config.DepthMode.AUTOMATIC
+                    } else {
+                        Config.DepthMode.DISABLED
+                    }
+
+                // Instant Placement is used if it is configured in Hello AR's settings.
+                instantPlacementMode =
+                    if (instantPlacementSettings.isInstantPlacementEnabled) {
+                        Config.InstantPlacementMode.LOCAL_Y_UP
+                    } else {
+                        Config.InstantPlacementMode.DISABLED
+                    }
+            }
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        results: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, results)
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            // Use toast instead of snackbar here since the activity will exit.
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                .show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        FullScreenHelper.setFullScreenOnWindowFocusChanged(this, hasFocus)
+    }
+    @Composable
+    fun OpenGLView() {
+        AndroidView(factory = {
+            view.surfaceView
+        })
+    }
+
+    @Composable
+    fun ARview() {
+        XRStudyWatchAppTheme {
+            OpenGLView()
+            MyUI {
+
+            }
+        }
+    }
+    @Composable
+    fun MyUI(onButtonClick: () -> Unit) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(text = "Welcome to My App", modifier = Modifier.padding(bottom = 8.dp))
+            Button(onClick = { onButtonClick() }) {
+                Text(text = "Click Me!")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    XRStudyWatchAppTheme {
+        Greeting("Android")
+    }
+}
